232d12c5f99a331acf06fc322d4aab84a2b5945a
9291Sam <sam.skinr@gmail.com>
2023-02-09 23:52:32 -0500

    sync

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -6,7 +6,7 @@ fn between<'a>(source: &'a str, start: &'a str, end: &'a str) -> Option<&'a str>
 
         let source = &source[start_position..];
         let end_position = source.find(end)?;
-        
+
         Some(&source[..end_position])
     }
     else
@@ -39,7 +39,8 @@ pub enum TokenizationError
 
 impl std::fmt::Display for TokenizationError
 {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
+    {
         match *self
         {
             TokenizationError::UnclosedDelimiter => write!(f, "Unclosed Delimiter!")
@@ -65,23 +66,21 @@ impl<'s> std::fmt::Debug for Token<'s>
             Self::DoubleColon => write!(f, "~::"),
             Self::EndOfFile => write!(f, "~EOF"),
             Self::StringLiteral(s) => write!(f, "~\"{s}\""),
-            Self::Identifier(s) => write!(f, "|{s}|"),
+            Self::Identifier(s) => write!(f, "|{s}|")
         }
     }
 }
-
 
 impl<'s> Token<'s>
 {
     fn try_get_keyword(string: &str) -> Option<(&str, Token)>
     {
         macro_rules! keyword {
-            ($working_string: ident, $match_string: literal, $match_token: expr) => {
+            ($working_string:ident, $match_string:literal, $match_token:expr) => {
                 if let Some(s) = $working_string.strip_prefix($match_string)
                 {
                     return Some((s, $match_token));
                 }
-
             };
         }
 
@@ -109,18 +108,17 @@ impl<'s> Token<'s>
         {
             return Ok((s, t));
         }
-        
+
         // Literals
         if string.starts_with('\"')
         {
             let found_literal: &str = match between(string, "\"", "\"")
             {
                 Some(s) => s,
-                None => return Err((TokenizationError::UnclosedDelimiter, string)),
+                None => return Err((TokenizationError::UnclosedDelimiter, string))
             };
             return Ok((&string[found_literal.len() + 2..], Token::StringLiteral(found_literal)));
         }
-
 
         // Identifier
         // keep incrementing the string up until we find another keyword
@@ -135,36 +133,43 @@ impl<'s> Token<'s>
 
             if string[..identifier_size].ends_with([' ', '\n'])
             {
-                return Ok((&string[identifier_size - 1..], Token::Identifier(&string[..identifier_size - 1])));
+                return Ok((
+                    &string[identifier_size - 1..],
+                    Token::Identifier(&string[..identifier_size - 1])
+                ));
             }
 
-            if let Some((_, _)) = Self::try_get_keyword(&string[identifier_size..])
+            if let Some((..)) = Self::try_get_keyword(&string[identifier_size..])
             {
-                return Ok((&string[identifier_size..], Token::Identifier(&string[..identifier_size])));
+                return Ok((
+                    &string[identifier_size..],
+                    Token::Identifier(&string[..identifier_size])
+                ));
             }
 
             identifier_size += 1;
         }
     }
 
+    // TODO: return a Vec<(Token, usize)> as a marker of where we are in the string
     pub fn parse(string: &str) -> Result<Vec<Token>, (TokenizationError, &str)>
     {
         let mut output: Vec<Token> = Vec::new();
 
         let mut rest_str: &str = string;
 
-        loop 
+        loop
         {
             if rest_str.is_empty()
             {
                 break;
             }
             let (next_str, t) = Token::find(rest_str)?;
-            
+
             rest_str = next_str.trim_start();
-            output.push(t); 
+            output.push(t);
         }
-        
+
         Ok(output)
     }
 }
