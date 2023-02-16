adf28ecbd6c47e1ac041a1c7d9794427b14f3dc7
9291Sam <sam.skinr@gmail.com>
2023-02-16 12:16:44 -0500

    more lexer work

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,48 +1,123 @@
-use std::{slice::{Iter, IterMut}, rc::Rc, cell::{UnsafeCell, Cell, RefCell}, borrow::BorrowMut, fs::File};
+use std::{slice::Iter, iter::Peekable, fs::File};
 
 use crate::tokenizer::{FileToken, Token};
 
-pub enum Lexeme<'s> {
+pub enum Lexeme<'s>
+{
     Import(&'s str),
+    Type(&'s str),
     FunctionDefinition { name: &'s str, data: Vec<Lexeme<'s>> },
     FunctionCall { name: &'s str, args: Vec<Lexeme<'s>> },
 }
 
 pub enum LexingError<'a>
 {
+    NoRemainingTokens,
     UnexpectedToken(FileToken<'a>)
 }
 
-fn lex_function<'a>(iterator: &mut Iter<FileToken>) -> Result<Lexeme<'a>, LexingError<'a>>
-{
-    todo!();
+macro_rules! next_should_be {
+    ($iterator: expr, $token: pat) =>
+    {
+        {
+            let next_file_token = $iterator.next()
+                .ok_or_else(|| LexingError::NoRemainingTokens)?;
+
+            match next_file_token.token
+            {
+                $token => Ok(next_file_token),
+                _ => Err(LexingError::UnexpectedToken(next_file_token.clone())),
+            }
+        }
+    };
 }
 
-fn lex_import<'a>(iterator: &mut Iter<FileToken>) -> Result<Lexeme<'a>, LexingError<'a>>
+fn lex_function<'a>(iterator: &mut Peekable<Iter<FileToken<'a>>>) -> Result<Lexeme<'a>, LexingError<'a>>
+{
+    next_should_be!(iterator, Token::Fn)?;
+
+    let name = next_should_be!(iterator, Token::Identifier(_))?;
+
+    // let args = {
+    //     // todo!();
+    // };
+
+    let _ = next_should_be!(iterator, Token::ThinArrow)?;
+    let return_type = next_should_be!(iterator, Token::Identifier(_))?
+    // };
+
+    
+
+    // // try to either parse a return tyoe x
+
+    // let return_type = {
+        
+    // };
+    
+
+
+
+    // let function_tokens: Vec<&FileToken> = Vec::new();  
+
+    // loop
+    // {
+    //     let current_file_token = iterator.next().ok_or_else(|| LexingError::NoRemainingTokens)?;
+
+    //     match current_file_token.token
+    //     {
+    //         Token::Import => todo!(),
+    //         Token::Fn => todo!(),
+    //         Token::LeftParen => todo!(),
+    //         Token::RightParen => todo!(),
+    //         Token::LeftBrace => todo!(),
+    //         Token::RightBrace => todo!(),
+    //         Token::LeftCurlyBrace => todo!(),
+    //         Token::RightCurlyBrace => todo!(),
+    //         Token::ThinArrow => todo!(),
+    //         Token::DoubleColon => todo!(),
+    //         Token::SemiColon => todo!(),
+    //         Token::EndOfFile => todo!(),
+    //         Token::StringLiteral(_) => todo!(),
+    //         Token::Identifier(_) => todo!(),
+    //     }
+    //     // add on new braces
+    //     // decrement on old braces
+        
+    // }
+    
+    // // {
+    // //     if 
+    // // }
+    // // Ok(Lexeme::FunctionDefinition { name: iter, data: () })
+    todo!()
+}
+
+fn lex_import<'a>(iterator: &mut Peekable<Iter<FileToken>>) -> Result<Lexeme<'a>, LexingError<'a>>
 {
     todo!();
 }
 
 impl<'s> Lexeme<'s>
 {
-    pub fn lex(tokens: Vec<FileToken>) -> Result<Vec<Lexeme<'s>>, LexingError>
+    pub fn lex(tokens: Vec<FileToken<'_>>) -> Result<Vec<Lexeme<'_>>, LexingError>
     {
         let mut output: Vec<Lexeme> = Vec::new();
-        let mut next: Iter<FileToken> = tokens.iter();
+        let mut next: Peekable<Iter<FileToken>> = tokens.iter().peekable();
         
         loop
         {
             output.push(
-                match next.next()
+                match next.peek()
                 {
-                    Some(global_token) =>
+                    Some(token) => 
                     {
-                        match global_token.token
+                        let file_token = token;
+                        match file_token.token
                         {
                             Token::Import    => lex_import(&mut next)?,
                             Token::Fn        => lex_function(&mut next)?,
                             Token::EndOfFile => break,
-                            _                => return Err(LexingError::UnexpectedToken(global_token.clone()))
+                            _                => return Err(LexingError::UnexpectedToken((*file_token).clone()))
                         }
                     },
                     None => todo!(),
